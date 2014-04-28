9cf8d2daad4310b8b607cefd7714e63b59fd163e
John Noss <noss@harvard.edu>
2014-04-28 17:20:09 -0400

    ordering of ops, and guest hostmanager

--- 05_swiftup.sh
+++ 05_swiftup.sh
@@ -2,3 +2,4 @@
 # If you want to test Swift, this is the script to run. It will bring up all of the
 # nodes necessary to run Swift.
 vagrant up --provider virtualbox puppet control swiftstore1 swiftstore2 swiftstore3
+vagrant hostmanager
